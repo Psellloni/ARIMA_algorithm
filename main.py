148792de7dfcdce0f6232ad204aa36739e00a9a0
levkasenko <kasenkolev318@gmail.com>
2023-09-12 21:19:09 +0300

    added pdmarima for one variable

--- main.py
+++ main.py
@@ -3,41 +3,55 @@ import pandas as pd
 import polars as pl
 import os
 
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_percentage_error as mape
+import pmdarima as pm
+
 
 def read_some_file():
-    abs_path = input('Enter absolute path: ')
+    file = input('Enter absolute path: ')
 
-    file_name, file_type = os.path.splitext(abs_path)
+    file_name, file_type = os.path.splitext(file)
 
     if file_type == '.csv':
-        df = 'csv'
-
-        #df = pd.DataFrame(data=pd.read_csv(abs_path))
+        data = pd.read_csv(file)
+        df = pd.DataFrame(data=data)
 
         return df
     elif file_type == '.parquet':
-        df = 'parquet'
-
-        #data = pl.read_ipc(abs_path)
-        #df = pd.DataFrame(data=data.to_pandas())
+        data = pl.read_ipc(file)
+        df = pd.DataFrame(data=data.to_pandas())
 
         return df
 
     return None
 
+def test_model(df: pd.DataFrame):
+    if df.shape[1] == 1:
+        train, test = train_and_test_split(df)
+
+        model = pm.auto_arima(train)
+
+        prediction = model.predict(n_periods=len(test))
+
+        print(mape(test, prediction))
+
+
+
 
 def train_and_test_split(df: pl.DataFrame):
-    days = input('Enter a number of days to be predicted: ')
+    n_values = int(input('Enter a number of values to be predicted: '))
 
-    train = df[:(len(df) - days)]
-    test = df[(len(df) - days):]
+    train = df[:(len(df) - n_values)]
+    test = df[(len(df) - n_values):]
 
     return train, test
 
 
+
 def main():
      df = read_some_file()
-     train, test = train_and_test_split(df)
+     test_model(df)
 
 if __name__ == '__main__':
     main()
