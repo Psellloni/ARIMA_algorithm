45db9a2ccd61dfd4c410ead6a83013717d45ed7a
levkasenko <kasenkolev318@gmail.com>
2023-09-13 13:04:10 +0300

    added arima model and linear regression for several variables(fixed)

--- main.py
+++ main.py
@@ -42,7 +42,28 @@ def train_and_test_split(df: pd.DataFrame, df_corr: pd.DataFrame, col_name: str)
 
     return train, test, n_values, real_df
 
-def test_model(df: pd.DataFrame, df_corr: pd.DataFrame):
+def test_x_model_prediction(train: pd.DataFrame, test: pd.DataFrame,
+                            n_values: int, col_name: str, real_df: pd.DataFrame):
+    # devide train to x and y
+    train_x = train.drop(col_name, axis=1)
+
+    # devide test to x and y
+    test_x = test.drop(col_name, axis=1)
+
+    df_pred = pd.DataFrame()
+
+    for col in train_x.columns:
+        model = pm.auto_arima(train_x[col])
+        prediction = model.predict(n_periods=n_values)
+
+        if mape(test_x[col], prediction) <= 0.9:
+            model2 = pm.auto_arima(real_df[col])
+            prediction2 = model2.predict(n_periods=n_values)
+            prediction2 = pd.Series(prediction2)
+            df_pred = pd.DataFrame(prediction2)
+    return df_pred
+
+def test_y_model(df: pd.DataFrame, df_corr: pd.DataFrame):
     # choosing column to be predicted
     print(df.columns)
     col_name = input(f'choose column to be predicted: ')
@@ -54,8 +75,10 @@ def test_model(df: pd.DataFrame, df_corr: pd.DataFrame):
         model = pm.auto_arima(train[col_name])
         prediction = model.predict(n_periods=len(test))
 
-        print(f'mape: {mape(test[col_name], prediction)}')
+        print(f'ARIMA mape: {mape(test[col_name], prediction)}')
         model_final = 'AR'
+
+        df_pred = 0
     else:
         # building a linear regression model and making a prediction
         model = LinearRegression()
@@ -78,30 +101,29 @@ def test_model(df: pd.DataFrame, df_corr: pd.DataFrame):
             print(f'ARIMA mape: {mapa2}')
             model_final = 'AR'
 
-    return model_final, col_name, n_values, real_df
+        df_pred = test_x_model_prediction(train, test, n_values, col_name, real_df)
+
+    return model_final, col_name, n_values, real_df, df_pred
 
 def building_model(model_final: str, df: pd.DataFrame,
-                   col_name: str, n_values: int, real_df: pd.DataFrame):
+                   col_name: str, n_values: int, real_df: pd.DataFrame, df_pred: pd.DataFrame):
     if model_final == 'LR':
-        #maling predictions for features using arima
-        model_x = pm.auto_arima(real_df.drop(col_name, axis=1))
-        prediction_x = model_x.predict(n_periods=n_values)
-        prediction_x = pd.DataFrame(data=prediction_x)
-
-        #making prediction for y using LinearRegression
         model_y = LinearRegression()
         model_y.fit(real_df.drop(col_name, axis=1), real_df[col_name])
-        prediction_y = model_y.predict(prediction_x)
+        prediction_y = model_y.predict(df_pred)
 
-        print(prediction_y)
+        return prediction_y
     elif model_final == 'AR':
         if real_df.shape[1] == 1:
             model = pm.auto_arima(real_df[col_name])
             prediction = model.predict(n_periods=n_values)
 
-            print(prediction)
+            return prediction
         else:
-            print('ligma ballz')
+            model = pm.auto_arima(real_df[col_name], real_df.drop(col_name, axis=1))
+            prediction = model.predict(n_periods=n_values, X=df_pred)
+
+            return prediction
 
 def main():
      df = read_some_file()
@@ -109,8 +131,10 @@ def main():
      #making df of correlations
      df_corr = df.corr()
 
-     model_final, col_name, n_values, real_df = test_model(df, df_corr)
-     building_model(model_final, df, col_name, n_values, real_df)
+     model_final, col_name, n_values, real_df, df_pred = test_y_model(df, df_corr)
+     prediction = building_model(model_final, df, col_name, n_values, real_df, df_pred)
+
+     print(prediction)
 
 if __name__ == '__main__':
     main()
